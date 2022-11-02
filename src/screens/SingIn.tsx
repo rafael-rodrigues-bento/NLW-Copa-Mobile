3af88d31c42afb5d63ad638689776d979044dd9e
Rafael Rodrigues Bento <rafael.rodrigues.bento@outlook.com>
2022-11-02 02:00:33 -0300

    Finalizada Aula 01

---
+++ src/screens/SingIn.tsx
@@ -0,0 +1,11 @@
+import { Text, Center } from "native-base";
+
+export function SingIn(){
+  return (
+    <Center flex={1} bgColor="gray.900" >
+      <Text color="white" fontSize={24} fontFamily="heading">
+        SignIn
+      </Text>
+    </Center>
+  )
+}
